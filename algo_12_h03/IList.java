64f0d12ea36a51c416b2f469b67b3a38e14f95c9
NikoSakic <75498612+NikoSakic@users.noreply.github.com>
2022-04-09 11:48:09 +0200

    Add files via upload

---
+++ algo_12_h03/IList.java
@@ -0,0 +1,10 @@
+
+public interface IList {
+
+	public void insertAt(int value, int pos);
+	public void removeAt(int pos);
+	public int getAt(int pos);
+	public int search(int value);
+	public void clear();
+	public int getCount();
+}
